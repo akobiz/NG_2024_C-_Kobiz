36e6c5b13aaa4182b3da47e6c5b7abaea2d5083d
Andrew Kobiz <andrewkobiz@gmail.com>
2024-06-27 12:28:30 +0300

    removed inheritance from person class

--- Epic_1_1/Classes/Person.cs
+++ Epic_1_1/Classes/Person.cs
@@ -1,15 +1,32 @@
 ﻿
 namespace Epic_1_1.Classes
 {
-    public class Person : Client
+    public class Person
     {
         public int PersonId { get; set; }
         public string Address { get; set; }
+        private Client Client_ { get; set; }
 
-        public Person(int personId, string address, int clientId, string clientName, int accountNumber) : base(clientId, clientName, accountNumber)
+        public Person(int personId, string address, int clientId, string clientName, int accountNumber)
         {
             PersonId = personId;
             Address = address;
+            Client_ = new Client(clientId, clientName, accountNumber);
+        }
+        public void Deposit(float amount)
+        {
+            Client_.Deposit(amount);
+        }
+
+        public void Withdraw(float amount)
+        {
+            Client_.Withdraw(amount);
+        }
+
+        public void GetBalance()
+        {
+            Client_.GetBalance();
         }
     }
 }
+
