f0108cc36974bffc33b35cc1aaebad444f7efd61
Andrew Kobiz <andrewkobiz@gmail.com>
2024-07-02 11:40:01 +0300

    Changed inherits between Person and Client classes

--- Epic_1_1/Classes/Client.cs
+++ Epic_1_1/Classes/Client.cs
@@ -2,18 +2,20 @@
 
 namespace Epic_1_1.Classes
 {
-    public class Client
+    public class Client : Person
     {
         public int ClientId { get; set; }
         public string ClientName { get; set; }
         public int AccountNumber { get; set; }
         private Balance ClientBalance;
 
-        public Client(int clientId, string clientName, int accountNumber)
+        public Client(int clientId, string clientName, int accountNumber, int personId, string address) : base(personId, address)
         {
             ClientId = clientId;
             ClientName = clientName;
             AccountNumber = accountNumber;
+            PersonId = personId;
+            Address = address;
             ClientBalance = new Balance(0, ClientId, 0);
         }
         public void Deposit(float amount)
--- Epic_1_1/Classes/Person.cs
+++ Epic_1_1/Classes/Person.cs
@@ -5,28 +5,11 @@ namespace Epic_1_1.Classes
     {
         public int PersonId { get; set; }
         public string Address { get; set; }
-        private Client Client_ { get; set; }
 
-        public Person(int personId, string address, int clientId, string clientName, int accountNumber)
+        public Person(int personId, string address)
         {
             PersonId = personId;
             Address = address;
-            Client_ = new Client(clientId, clientName, accountNumber);
-        }
-        public void Deposit(float amount)
-        {
-            Client_.Deposit(amount);
-        }
-
-        public void Withdraw(float amount)
-        {
-            Client_.Withdraw(amount);
-        }
-
-        public void GetBalance()
-        {
-            Client_.GetBalance();
         }
     }
 }
-
